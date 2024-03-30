3a3b8cba11f8e33cef0f3263e1ff3889b90c10fa
Benjamin Feurly <benji.feu.feu.feu@gmail.com>
2024-03-30 15:42:43 +0100

    Moved easy script code to specific classes

---
+++ src/CommandUtils.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Vitafeu\EasyMVC;
+
+use Vitafeu\EasyMVC\Maker;
+use Vitafeu\EasyMVC\Database;
+
+class CommandUtils {
+    public function handleCommand($args) {
+        array_shift($args);
+        $command = array_shift($args);
+
+        if (empty($args)) {
+            $command = 'help';
+        }
+        
+        switch ($command) {
+            case 'serve':
+                echo "Starting server on http://localhost:8080 \n";
+                exec('php -S localhost:8080 -t public');
+                break;
+            case 'make:controller':
+                Maker::makeController(array_shift($args));
+                break;
+            case 'make:model':
+                $name = array_shift($args);
+                Maker::makeModel($name);
+
+                // check for controller flag
+                $nextArg = array_shift($args);
+                if ($nextArg === '-c' || $nextArg === '--controller') {
+                    Maker::makeController($name);
+                }
+                break;
+            case 'make:tables':
+                Database::createTables();
+                break;
+            case 'drop:tables':
+                Database::dropTables();
+                break;
+            case 'refresh:tables':
+                Database::dropTables();
+                Database::createTables();
+                break;
+            case 'help':
+                echo "Available commands:\n";
+                echo "  make:controller <name>\n";
+                echo "  make:model <name>\n";
+                echo "  make:tables\n";
+                echo "  drop:tables\n";
+                break;
+            default:
+                echo "Unknown command: $command\n";
+                echo "Type 'help' for a list of available commands.\n";
+                break;
+        }
+    }
+}
---
+++ src/Maker.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Vitafeu\EasyMVC;
+
+use Vitafeu\EasyMVC\Globals;
+
+class Maker {
+    public static function makeController($name)
+    {
+        $name = ucfirst($name);
+        $template = <<<EOT
+                    <?php
+    
+                    namespace App\Controllers;
+    
+                    use Vitafeu\EasyMVC\Controller;
+    
+                    class {$name}Controller extends Controller {
+                        public function index() {   
+                            \$this->render('$name/Home');
+                        }
+                    }
+                    EOT;
+    
+        $fileName = Globals::getProjectRoot() . "app/Controllers/{$name}Controller.php"; 
+        
+        if (!file_exists($fileName)) {
+            file_put_contents($fileName, $template);
+            echo "$name controller created successfully\n";
+        } else {
+            echo "$name controller already exists\n";
+        }
+    }
+    
+    public static function makeModel($name)
+    {
+        $name = ucfirst($name);
+        $lowerName = strtolower($name);
+        $template = <<<EOT
+                    <?php
+    
+                    namespace App\Models;
+    
+                    use Vitafeu\EasyMVC\Model;
+    
+                    class {$name} extends Model {
+                        protected static \$table = '{$lowerName}s';
+    
+                        protected static \$attributes = [
+                            'id' => 'INT AUTO_INCREMENT PRIMARY KEY',
+                            // Complete with your own attributes
+                        ];
+                    }
+                    EOT;
+    
+
+        $rootDir = Globals::getProjectRoot();
+        $fileName = $rootDir . "app/Models/{$name}.php"; 
+    
+        if (!file_exists($rootDir . "/app/Models")) {
+            mkdir($rootDir . "/app/Models");
+            echo "Created Models folder\n";
+        }
+    
+        if (!file_exists($fileName)) {
+            file_put_contents($fileName, $template);
+            echo "$name model created successfully\n";
+        } else {
+            echo "$name model already exists\n";
+        }
+    }
+}
